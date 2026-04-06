463dd388a7e10be0b39f72d57015c8dde2ee4342
Saket Pandey <nikhilpandey.pandey9@gmail.com>
2026-04-06 19:39:45 +0530

    build

---
+++ dist/build/CreatePage-CH-Guy5X.js
@@ -0,0 +1,1 @@
+import{a_ as r,j as e,P as o}from"./strapi-BCpeuz29.js";import{E as i}from"./EditPage-CukrmGxs.js";import"./selectors-BG6WlD6s.js";import"./useWebhooks-DE29Sp-w.js";const c=()=>{const s=r(t=>t.admin_app.permissions.settings?.webhooks.create);return e.jsx(o.Protect,{permissions:s,children:e.jsx(i,{})})};export{c as ProtectedCreatePage};
---
+++ dist/build/hooks-BukNsQ4X.js
@@ -0,0 +1,1 @@
+import{l as e}from"./strapi-BCpeuz29.js";const r=e;export{r as u};
